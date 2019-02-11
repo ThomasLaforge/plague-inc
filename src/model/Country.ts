c37e0a36875563acbf29e4f0fb25d28d58062fc6
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-02-11 18:28:38 +0100

    start country implementation

--- src/model/Country.ts
+++ src/model/Country.ts
@@ -1,5 +1,36 @@
+import { Player } from "./Player";
+import uniq from 'lodash/uniq'
 export class Country {
-    constructor(name: string, nbSlots: number) {
-        
+    public name: string
+    public nbSlots: number
+    public infections: Player[]
+
+    constructor(name: string, nbSlots: number, infections: Player[] = []) {
+        this.name = name
+        this.nbSlots = nbSlots
+        this.infections = infections
+    }
+
+    addInfection(player: Player, nb = 1){
+        if(this.infections.length + nb > this.nbSlots){
+            throw Error('try to add too many infections')
+        }
+        let newInfections = new Array(nb).fill(player)
+        this.infections = this.infections.concat(newInfections)
+    }
+    removeInfection(player: Player, nb = 1){
+        // TODO
+    }
+
+    getInfectors(): Player[] {
+        return uniq(this.infections)
+    }
+
+    getTopInfectors(): Player[] {
+        return []
+    }
+
+    isFullyInfected(){
+        return this.infections.length === this.nbSlots
     }
 }
